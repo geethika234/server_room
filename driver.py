0c6b42709dcd8dfeb732e8e40818a7702b0a7880
geethika234 <31622964+geethika234@users.noreply.github.com>
2019-06-24 13:26:59 +0530

    Update driver.py

--- driver.py
+++ driver.py
@@ -1,11 +1,13 @@
 import os
 import sys
-import shutil
 import pickle
-import glob
 from temp_detect import TempDetect
 from send_sms import SendSMS
-from send_mail import SendEmail
+import groupmail as gmi
+import groupnorm as gm
+import csv
+import time
+
 
 class Driver():
 	def __init__(self):
@@ -13,67 +15,54 @@ class Driver():
 		self.contacts_dict = pickle.load(dictfile)
 		dictfile.close()
 		self.smsobj = SendSMS()
-		self.mailobj = SendEmail()
-
 	def get_image(self):
-		list_of_files = glob.glob('/home/pi/Pictures/server/dark/*')
-		latest_file = max(list_of_files, key=os.path.getctime)
+		latest_file = '/home/pi/Pictures/server/image.jpg'
 		return latest_file
 
 	def detect_digit(self):
 		subject = 'Temperature Alert'
 		negmsg = 'Unable to detect successfully. Detects: '
-		alertmsg = 'Temperature beyond acceptable range. The display reads: '
+		alertmsg1 = 'Temperature beyond acceptable range. The display reads : '
+		alertmsg2 = 'Temperature beyond acceptable range. The sensor reads : '
+		add = ' and sensor reads: '
 		img_path = self.get_image()
 		tobj = TempDetect(img_path=img_path)
 		digits = []
-
 		try:
-			# First attempt to detect. cv2.dilate() value = 1
-			digits = tobj.final_call(iterval=1)
-			digits_str = u'{}{}'.format(*digits)
-
-			# Unable to map to a digit (returns -1)
-			if -1 in digits:
-				# Second attempt to detect. cv2.dilate() value = 2
-				digits = tobj.final_call(iterval=2)
-				digits_str = u'{}{}'.format(*digits)
-
-				# Unable to detect second time
-				if -1 in digits:
-					for key in self.contacts_dict:
-						self.mailobj.sendEmail(self.contacts_dict[key]['mailid'], subject, negmsg, img_path)
-						self.smsobj.sendSMS(negmsg, self.contacts_dict[key]['mnum'])
-					sys.exit()
-
-				#Else, successfully detected (2nd attempt)
-			#Else, successfully detected (1st attempt)
-
-		# Unable to detect both the digits correctly
-		except IndexError:
+			# First attempt to detect. cv2.dilate() value = 2
 			digits = tobj.final_call(iterval=2)
-			# Still unable to detect 2nd time (either -1 or misses one digit)
-			if -1 in digits or len(digits)<2:
-				for key in self.contacts_dict:
-					self.mailobj.sendEmail(self.contacts_dict[key]['mailid'], subject, negmsg, img_path)
-					if self.contacts_dict[key]['mnum'] != '':
-						self.smsobj.sendSMS(negmsg, self.contacts_dict[key]['mnum'])
-
-
-		digits2 = digits[:2]
-		digits_str = ''.join(str(x) for x in digits2)
-
+			# digits2 = digits[:2]
+			digits_str = ''.join(str(x) for x in digits)
+			self.disp=digits_str
+			# Unable to map to a digit (returns -1) or detects no digit
+			if -1 in digits or len(digits) < 2 or len(digits) > 2:
+				message = negmsg+(digits_str[:3])
+				gmi.group(['patchavageethika@gmail.com','butyg333@gmail.com'], message, subject)
+		except Exception as exception:
+			msg = 'ERROR: ' + type(exception).__name__ + str(exception)
+			gmi.group(['patchavageethika@gmail.com','butyg333@gmail.com'], message, subject)
+			
+			message = alertmsg1+(digits_str[:3]) + add+str(sens)
+			li = []
 		for key in self.contacts_dict:
-        	if not self.contacts_dict[key]['mint'] < digits_str < self.contacts_dict[key]['maxt']:
-	            self.mailobj.sendEmail(self.contacts_dict[key]['mailid'], subject, alertmsg, moved_to)
-	            if self.contacts_dict[key]['mnum'] != '':
-					self.smsobj.sendSMS(alertmsg, self.contacts_dict[key]['mnum'])
-
-			else:
-				# Within the given range. Do nothing.
-				pass
-
+			# print(key)
+			if self.contacts_dict[key]['status'] != 'Inactive':
+				if (not (int(self.contacts_dict[key]['mint']) <= int(digits_str) <= int(self.contacts_dict[key]['maxt']))) or (not (int(self.contacts_dict[key]['mint']) <= sens <= int(self.contacts_dict[key]['maxt']))):
+					# print("mail sent")
+					li.append(self.contacts_dict[key]['mailid'])
+					if self.contacts_dict[key]['mnum'] != '':
+						elf.smsobj.sendSMS(message, self.contacts_dict[key]['mnum'])
+			gmi.group(li, message, subject)
+			
+	def move_to_csv(self):
+			t=time.localtime()
+			row=[str(t.tm_year)+"-"+str(t.tm_mon)+"-"+str(t.tm_mday)+"/"+str(t.tm_hour)+"-"+str(t.tm_min),self.disp,str(self.sensor)]
+			with open('data.csv', 'a') as csvFile:
+				writer = csv.writer(csvFile)
+				writer.writerow(row)
+			csvFile.close()
 
 
 mainobj = Driver()
 mainobj.detect_digit()
+mainobj.move_to_csv()
